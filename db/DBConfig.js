68ad6c899f83b99525d063b39940bbed760c93a2
dupengtao123 <2030055246@qq.com>
2017-05-22 20:29:40 +0800

    留言板

---
+++ db/DBConfig.js
@@ -0,0 +1,10 @@
+module.exports =
+ {  
+             mysql: {   
+                         host: '127.0.0.1',     
+                         user: 'root',   
+                         password: '你的数据库Coonection密码',  
+                         database:'ExpressDB', // 前面建的user表位于这个数据库中 
+                          port: 3306  
+                    }
+ };
